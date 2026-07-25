79502e7ad2a725acd4d184a64f51f9e088419c0b
Musa Adam Babatunde <musaadambabatunde@gmail.com>
2026-07-25 14:05:27 +0100

    Configuration update for deployment

--- propsafe/build.sh
+++ propsafe/build.sh
@@ -2,10 +2,11 @@
 # exit on error
 set -o errexit
 
-cd propsafe
 
 python -m pip install --upgrade pip
-pip install -r ./requirements.txt
+pip install -r requirements.txt
+
+cd propsafe
 
 python manage.py collectstatic --no-input
 python manage.py migrate
